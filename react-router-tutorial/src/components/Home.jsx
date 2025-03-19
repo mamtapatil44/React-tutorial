f15530ea03e0f78835d128e2ab824a98607ede09
Mamta Patil <mamtapatil44@gmail.com>
2025-03-19 12:13:40 -0400

    btns

--- react-router-tutorial/src/components/Home.jsx
+++ react-router-tutorial/src/components/Home.jsx
@@ -4,13 +4,29 @@ function Button({ label, ...handlers }) {
   return <button {...handlers} className="bg-red-400 p-2 rounded-lg">{label}</button>;
 }
 const Home = () => {
-  const dynamicEvents = ["Click", "MouseEnter", "Focus"];
-  const handlers = dynamicEvents.reduce((acc, event) => {
-    acc[`on${event}`] = () =>
-      console.log(`Button was ${event.toLowerCase()}ed!`);
-    return acc;
-  }, {});
-  return <Button label="Multi-Event Button" {...handlers}  />;
+  // const dynamicEvents = ["Click", "MouseEnter", "Focus"];
+  // const handlers = dynamicEvents.reduce((acc, event) => {
+  //   acc[`on${event}`] = () =>
+  //     console.log(`Button was ${event.toLowerCase()}ed!`);
+  //   return acc;
+  // }, {});
+  // return <Button label="Multi-Event Button" {...handlers}  />;
+
+  //  2nd approach using map and [key ]notation
+  const buttons = [
+    { label: "Click Me", event: "Click" },
+    { label: "Hover Me", event: "MouseEnter" },
+    { label: "Focus Me", event: "Focus" }
+  ];
+
+  return (
+    <div>
+      {buttons.map(({ label, event }) => {
+        const handlers = { [`on${event}`]: () => alert(`${label} was ${event.toLowerCase()}ed!`) };
+        return <Button key={label} label={label} {...handlers} />;
+      })}
+    </div>
+  );
 };
 
 export default Home;
